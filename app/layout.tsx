897633fe373556c715fffa873ea892b53705d401
thenet84 <the.net.wo@gmail.com>
2025-02-07 11:48:17 +0100

    feat: add theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/providers/ThemeProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,16 +27,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        <div className="flex">
-          <div className="hidden md:block h-[100vh] w-[300px]"><Sidebar /></div>
-          <div className="p-5 w-full md:max-[1140px]">{children}</div>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} storageKey="dashboard-theme">
+          <Navbar />
+          <div className="flex">
+            <div className="hidden md:block h-[100vh] w-[300px]"><Sidebar /></div>
+            <div className="p-5 w-full md:max-[1140px]">{children}</div>
+          </div>
           <Toaster />
-        </div>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,28 +9,32 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import ThemeToggler from "./ThemeToggler";
 
 const Navbar = () => {
     console.log('test');
     return <div className="bg-primary dark:bg-slate-700 py-2 px-5 text-white flex justify-between items-center">
         <Link href="/">
-            <Sparkles size="28"/>
-        </Link> 
+            <Sparkles size="28" />
+        </Link>
+        <div className="flex items-center">
+            <ThemeToggler />
+            <DropdownMenu>
+                <DropdownMenuTrigger className="focus:outline-none">
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                        <AvatarFallback className="text-black">CN</AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem><Link href="/profile">Profile</Link></DropdownMenuItem>
+                    <DropdownMenuItem><Link href="/auth">Logout</Link></DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
 
-        <DropdownMenu>
-            <DropdownMenuTrigger className="focus:outline-none">
-                <Avatar>
-                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                    <AvatarFallback className="text-black">CN</AvatarFallback>
-                </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem><Link href="/profile">Profile</Link></DropdownMenuItem>
-                <DropdownMenuItem><Link href="/auth">Logout</Link></DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
     </div>;
 }
 
